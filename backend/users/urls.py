b245a1e355976a7fbbb1e593b5fa85492d86b009
서지희 <sjh090524@naver.com>
2025-11-02 03:05:32 +0900

    feat(auth): implement email & google login APIs

---
+++ backend/users/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import RegisterView, LoginView, MeView, GoogleLoginView
+
+urlpatterns = [
+    path("register", RegisterView.as_view()),
+    path("login",    LoginView.as_view()),
+    path("me",       MeView.as_view()),
+    path("google",   GoogleLoginView.as_view()),
+]
